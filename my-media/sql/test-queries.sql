5a105082e392c80ccc62387dee2d673d257b4f75
Anthony Flowers <22030883+AnthonyFlowers@users.noreply.github.com>
2022-11-14 12:02:55 -0600

    updated schema to include an app_user and linking tables

--- my-media/sql/test-queries.sql
+++ my-media/sql/test-queries.sql
@@ -4,7 +4,15 @@ select * from tv_show_episode;
 select * from tv_show;
 select * from tv_show_season;
 select * from tv_show_episode e join
-	tv_show_season s join
+    tv_show_season s join
     tv_show t
-	on e.tv_show_season_id = s.tv_show_season_id
+    on e.tv_show_season_id = s.tv_show_season_id
     and s.tv_show_id = t.tv_show_id;
+
+select username, movie_name from app_user a join app_user_movie am join movie m
+    on a.app_user_id = am.app_user_id
+    and am.movie_id = m.movie_id;
+
+select username, tv_show_name from app_user a join app_user_tv_show ats join tv_show ts
+    on a.app_user_id = ats.app_user_id
+    and ats.tv_show_id = ts.tv_show_id;
--- my-media/sql/test.sql
+++ my-media/sql/test.sql
@@ -21,8 +21,8 @@ create table tv_show_season (
 );
 
 create table tv_show_season_tv_show (
-    tv_show_season_id int,
-    tv_show_id int,
+    tv_show_season_id int not null,
+    tv_show_id int not null,
     primary key(tv_show_season_id, tv_show_id),
     constraint fk_tv_show_season_tv_show_tv_show_season_tv_show_season_id
         foreign key (tv_show_season_id)
@@ -43,54 +43,139 @@ create table tv_show_episode (
         references tv_show_season(tv_show_season_id)
 );
 
+create table app_user (
+    app_user_id int primary key auto_increment,
+    username varchar(50) not null unique,
+    password_hash varchar(2048) not null,
+    enabled bit not null default(1)
+);
+
+create table app_user_movie (
+    app_user_id int not null,
+    movie_id int not null,
+    constraint pk_app_user_movie_u_id_m_id
+        primary key (app_user_id, movie_id),
+    constraint fk_app_user_movie_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_movie_movie_id
+        foreign key (movie_id)
+        references movie(movie_id)
+);
+
+create table app_user_tv_show (
+    app_user_id int not null,
+    tv_show_id int not null,
+    constraint pk_app_user_tv_show_u_id_tv_s_id
+        primary key (app_user_id, tv_show_id),
+    constraint fk_app_user_tv_show_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_tv_show_tv_show_id
+        foreign key (tv_show_id)
+        references tv_show(tv_show_id)
+);
+
+create table app_role (
+    app_role_id int primary key auto_increment,
+    `name` varchar(50) not null unique
+);
+
+create table app_user_role (
+    app_user_id int not null,
+    app_role_id int not null,
+    constraint pk_app_user_role
+        primary key (app_user_id, app_role_id),
+    constraint fk_app_user_role_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_role_role_id
+        foreign key (app_role_id)
+        references app_role(app_role_id)
+);
+
+insert into app_role (`name`) values
+    ('ADMIN'),
+    ('USER');
+
 delimiter //
 create procedure set_known_good_state()
 begin
     set sql_safe_updates = 0;
-    
-    delete from tv_show_season_tv_show;
+
+delete from app_user_role;
+    alter table app_user_role auto_increment = 1;
+delete from app_user_movie;
+delete from app_user_tv_show;
+delete from app_user;
+    alter table app_user auto_increment = 1;
+delete from tv_show_season_tv_show;
     alter table tv_show_season_tv_show auto_increment = 1;
-    delete from tv_show_episode;
+delete from tv_show_episode;
     alter table tv_show_episode auto_increment = 1;
-    delete from tv_show_season;
+delete from tv_show_season;
     alter table tv_show_season auto_increment = 1;
-    delete from tv_show;
+delete from tv_show;
     alter table tv_show auto_increment = 1;
-    delete from movie;
+delete from movie;
     alter table movie auto_increment = 1;
-    
+
     insert into movie (movie_name, movie_year, movie_length) values
-        ('Iron Man', 2008, 126),
-        ('Iron Man 2', 2010, 125),
-        ('Iron Man 3', 2013, 130);
+        ('Iron Man', 2008, 126), -- test read
+        ('Iron Man 2', 2010, 125), -- test update
+        ('Iron Man 3', 2013, 130); -- test delete
+        -- test create ('Spider-Man', null, 121)
 
     insert into tv_show (tv_show_name) values
-        ('Rick and Morty'),
-        ('Suits'),
-        ('Stranger Things');
+        ('Rick and Morty'), -- test read
+        ('Suits'), -- test update
+        ('Stranger Things'); -- test delete
+        -- test create ('Tokyo Ghoul')
 
     insert into tv_show_season (tv_show_id, tv_show_season_index) values
-        (1, 1), 
+        (1, 1),
         (1, 2),
         (1, 3),
-        (2, 1),
-        (2, 2),
-        (2, 3);
+        (2, 1), -- test update
+        (2, 2), -- test delete
+        (2, 3); -- test read
+        -- test create (1, 4)
 
     insert into tv_show_season_tv_show (tv_show_season_id, tv_show_id) values
         (1, 1),
         (2, 2);
 
     insert into tv_show_episode (tv_show_season_id, tv_show_episode_index) values
-        (1, 1), 
-        (1, 2),
+        (1, 1),
+        (1, 2), -- test read
         (1, 3),
-        (1, 4),
+        (1, 4), -- test update
         (4, 1),
         (4, 2),
-        (4, 3),
+        (4, 3), -- test delete
         (4, 4);
-        
+        -- test create (1, 5)
+
+    insert into app_user (username, password_hash) values
+        ('johnsmith', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa'),
+        ('janedoe', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa');
+
+    insert into app_user_movie (app_user_id, movie_id) values
+        (1, 1),
+        (1, 2),
+        (2, 1);
+
+    insert into app_user_tv_show (app_user_id, tv_show_id) values
+        (1, 1),
+        (1, 2),
+        (2, 2);
+
+    insert into app_user_role (app_user_id, app_role_id) values
+        (1, 1),
+        (2, 2);
+
     set sql_safe_updates = 1;
 end //
 delimiter ;
+
+call set_known_good_state();
