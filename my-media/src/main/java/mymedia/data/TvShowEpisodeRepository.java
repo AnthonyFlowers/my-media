121a9f72349f82766d9ae2c6bbef8ee5c75dd3e2
Anthony Flowers <22030883+AnthonyFlowers@users.noreply.github.com>
2022-12-12 13:33:27 -0600

    upgrading hibernate version

--- my-media/src/main/java/mymedia/data/TvShowEpisodeRepository.java
+++
@@ -1,9 +0,0 @@
-package mymedia.data;
-
-import mymedia.models.TvShowEpisode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TvShowEpisodeRepository extends JpaRepository<TvShowEpisode, Integer> {
-}
--- my-media/src/main/java/mymedia/data/TvShowSeasonRepository.java
+++
@@ -1,9 +0,0 @@
-package mymedia.data;
-
-import mymedia.models.TvShowSeason;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TvShowSeasonRepository extends JpaRepository<TvShowSeason, Integer> {
-}
